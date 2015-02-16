96036eeeb9c99b2686e988da2a20d763aa73cb67
Georgi Sabev <georgethebeatle@gmail.com>
2015-02-16 13:19:58 +0200

    Register registry with consul
    [#86969440]
    
    Signed-off-by: Hristo Iliev <hsiliev@gmail.com>

--- jobs/docker/templates/ctl.sh
+++ jobs/docker/templates/ctl.sh
@@ -29,7 +29,7 @@ case $1 in
       -p $PIDFILE \
       -g $DOCKER_DATA_DIR \
       -mtu 1500 \
-      --insecure-registry=localhost:5000 \
+      --insecure-registry=localhost:<%= p("docker_registry.address").split(":")[1] %> \
       1>>$LOG_DIR/docker.stdout.log \
       2>>$LOG_DIR/docker.stderr.log
 
